9ebb7c4ff0cc9d62e970903c8afebdf82454cb20
Do Tran <do.tran@saigontechnology.com>
2024-01-12 21:58:39 +0700

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tailwindnet8.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./Views/**/*.cshtml", "./Views/*.cshtml"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
